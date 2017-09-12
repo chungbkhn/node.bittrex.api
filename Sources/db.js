9a70ed3f5fed97ad141935330db8b530b917c7fe
Chung Duong <david@waltzcode.com>
2017-09-13 01:14:20 +0700

    add playground to calculate money

--- Sources/db.js
+++ Sources/db.js
@@ -1,67 +1,23 @@
-var MySQLAPI = function() {
-    'use strict';
+const mysql = require('mysql');
 
-	var mysql = require('mysql');
-	var isStartedConnection = false;
+console.log(mysql);
+exports.pool = mysql.createPool({
 
-    var con = mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "Bittrex"
-    });
+    host            : 'localhost',
+    user            : 'root',
+    password        : '',
+    database        : 'Bittrex'
+});
+console.log(exports.pool);
 
-    var startConnect = function(callback) {
-        con.connect(function(err) {
-          if (err) {
-            console.error('error connecting: ' + err.stack);
-            callback(isStartedConnection);
-            return;
-          }
-         
-          console.log('connected as id ' + con.threadId);
 
-          isStartedConnection = true;
+exports.create = function() {
 
-          callback(isStartedConnection);
-        });
+    console.log('init')
+    if (!exports.pool) {
+        console.log("create pool");
+
     }
+}
 
-    var stopConnect = function(callback) {
-        connection.end(function(err) {
-            if (err) {
-                console.error('error connecting: ' + err.stack);
-                return;
-            }
-
-            callback(true);
-        });
-    }
-
-    var dbAPICall = function(sql, callback) {
-      con.query(sql, function (err, result) {
-        if (err) throw err;
-
-        callback(result, err);
-      });
-    };
-
-    return {
-        getAllCandles30Min: function(callback) {
-            var sql = 'select * from Candle30Min';
-            dbAPICall(sql, callback);
-        },
-        insertCandles30Min: function(open, close, high, low, volume, timestamp, callback) {
-            var sql = 'insert into Candle30Min (open, close, high, low, volume) values (' + open + ', ' + close + ', ' + high + ', ' + low + ', ' + volume + ')';
-            dbAPICall(sql, callback);
-        },
-        startConnect: function (callback) {
-            startConnect(callback);
-        },
-        stopConnect: function (callback) {
-            stopConnect(callback);
-        }
-    };
-}();
-
-module.exports = MySQLAPI;
+exports.getConnection = exports.pool.getConnection
