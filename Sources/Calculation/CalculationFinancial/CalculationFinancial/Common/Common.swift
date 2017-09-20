017999f6290f327f58a1c328c6c9ef6953f9d5bf
Chung Duong <chungbkhn87@gmail.com>
2017-09-20 12:02:00 +0700

    Add Calculator project

---
+++ Sources/Calculation/CalculationFinancial/CalculationFinancial/Common/Common.swift
@@ -0,0 +1,15 @@
+//
+//  Common.swift
+//  CalculationFinancial
+//
+//  Created by Duong Van Chung on 9/20/17.
+//  Copyright © 2017 Duong Van Chung. All rights reserved.
+//
+
+import UIKit
+
+let backgroundQueue = DispatchQueue(label: "calculation.financial.backgroundqueue", attributes: .concurrent)
+
+class Common {
+
+}
